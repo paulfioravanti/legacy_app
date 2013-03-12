3e3adc4390be7c3d0275cc338ff233048ef3dd7b
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-03-12 17:37:51 +1030

    Change out depreciated cookie setting for config.secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '1ec298ff6718e3a65191886596ab7514a4e1d93d2bfcc1fed61d573fd95920ede49860413789092ac7e2c796ed2fe4a2a0deef5cb8b07ac9c7f662bd391b524f';
+LegacyApp::Application.config.secret_token = '7559847f08785b6f756c0d9ff79433eaa043892ff94f91cd19ecd6a74918f306d6ca03a951a32ea64ecfbe2875bd282f8084ae15dfb97ebe900749d30b20dc83'
