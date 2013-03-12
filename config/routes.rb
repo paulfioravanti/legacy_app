62d3a4c62c76898b83e5d5e1b0b884c6a00b0975
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-03-13 00:30:33 +1030

    Restrict generation of automatic routes to methods actually used in the controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 LegacyApp::Application.routes.draw do
   resources :images, except: [:show] do
-    resources :featured_images
+    resources :featured_images, only: [:new, :create]
   end
   root to: 'pages#home'
 end
