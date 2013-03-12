c07112e1ce610c02cb8c8e169b172b9985f6d650
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-03-12 17:31:40 +1030

    Change named_scope references to just scope, cleaned up

--- app/models/featured_image.rb
+++ app/models/featured_image.rb
@@ -1,14 +1,17 @@
 class FeaturedImage < ActiveRecord::Base
 
+  attr_accessible :feature_at
+
   belongs_to :image
 
   validates_presence_of :feature_at, :image_id
 
-  named_scope :before_date, lambda { |date| { :conditions => ['feature_at < ?', date] } }
+  scope :before_date,
+        lambda { |date| { conditions: ['feature_at < ?', date] } }
 
   def self.current
-    feature_image = self.before_date(Time.now).first(:order => 'feature_at DESC')
+    feature_image = self.before_date(Time.now).
+                    first(order: 'feature_at DESC')
     feature_image.try :image
   end
-
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,21 +1,22 @@
 class Image < ActiveRecord::Base
 
+  attr_accessible :uploaded_data, :caption
+
   cattr_accessor :thumbnail_sizes
   @@thumbnail_sizes = {
-    :medium => 512,
-    :small => 128,
-    :tiny => 64
+    medium: 512,
+    small: 128,
+    tiny: 64
   }
 
-  has_attachment :storage        => :file_system,
-                 :size           => 1..5.megabytes,
-                 :processor      => 'MiniMagick',
-                 :background     => true,
-                 :thumbnails     => Image.thumbnail_sizes
+  has_many :featured_images, dependent: :destroy
+
+  scope :without_parents, conditions: { parent_id: nil }
+
+  has_attachment storage:    :file_system,
+                 size:       1..5.megabytes,
+                 processor:  'MiniMagick',
+                 background: true,
+                 thumbnails: Image.thumbnail_sizes
   validates_as_attachment
-
-  has_many :featured_images, :dependent => :destroy
-
-  named_scope :parents, :conditions => {:parent_id => nil}
-
 end
